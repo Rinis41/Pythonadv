1c58f4c12debc79ffbec21f2b3a323d4551bd468
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2026-04-04 16:22:34 +0200

    yes

---
+++ Lesson9/polymorphism/inbuild_polymorphism.py
@@ -0,0 +1,19 @@
+str_length = len("Hello World")
+list_length = len([1,2,3,4])
+tuple_length = len((10,20,30))
+
+print(str_length)
+print(list_length)
+print(tuple_length)
+
+sum_of_list = sum([1,2,3,4,5])
+sum_of_tuple = sum((10, 5, 20))
+
+print(sum_of_tuple)
+print(sum_of_list)
+
+max_value = max(5, 10, 3)
+max_float = max(3.14, 2.71, 5.5)
+
+print(max_value)
+print(max_float)
