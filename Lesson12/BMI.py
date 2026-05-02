4984bcf750f22fc1d9e62a048b0302eb0e7c17da
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2026-05-02 15:42:36 +0200

    yes

---
+++ Lesson12/BMI.py
@@ -0,0 +1,96 @@
+from abc import ABC, abstractmethod
+
+
+class Person(ABC):
+    def __init__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+
+    @property
+    def weight(self):
+        return self.weight
+
+    @weight.setter
+    def weight(self, value):
+        if value < 0:
+            raise ValueError('Weight cannot be negative')
+        self.weight = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError('Height cannot be negative')
+        self.height = value
+
+    @abstractmethod
+    def calculate_bmi(self):
+        pass
+
+    @abstractmethod
+    def get_bmi_category(self):
+        pass
+
+    def print_info(self):
+        print(f"{self.name}, Age: {self.age}, Weight {self.weight} kg, Height: {self.height} m, "
+              f"BMI: {self.calculate_bmi():.2f}, Category: {self.get_bmi_category()}"
+        )
+
+class Adult(Person):
+    def calculate_bmi(self):
+        return self.weight / (self.height ** 2)
+
+    def get_bmi_category(self):
+        bmi = self.calculate_bmi()
+        if bmi < 18.5:
+            return "UnderWeight"
+        elif 18.5 <= bmi < 24.9:
+            return "Normal Weight"
+        elif 24.9 <= bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obese"
+
+class Child(Person):
+
+    def calculate_bmi(self):
+        return (self.weight / (self.height ** 2)) * 1.3
+
+    def get_bmi_category(self):
+        bmi = self.calculate_bmi()
+        if bmi < 14:
+            return "UnderWeight"
+        elif 14 <= bmi < 18:
+            return "Normal Weight"
+        elif 18 <= bmi < 24:
+            return "Overweight"
+        else:
+            return "Obese"
+
+class BMIApp:
+
+    def __init__(self):
+        self.people = []
+
+    def add_person(self, person):
+        self.people.append(person)
+
+    def collect_user_data(self):
+
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        weight = float(input("Enter your weight in kg: "))
+        height = float(input("Enter your height in meters: "))
+
+        if age >= 18:
+            person = Adult(name, age, weight, height)
+        else:
+            person = Child(name, age, weight, height)
+
+        self.add_person(person)
