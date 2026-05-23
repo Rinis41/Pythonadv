9ba374b92b2d9977134e08d8f9d7ab09c7b3971c
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2026-05-23 15:36:33 +0200

    Update bar_chart.py

--- Lesson14/bar_chart.py
+++ Lesson14/bar_chart.py
@@ -11,18 +11,18 @@ print(filtered_df)
 
 plt.figure(figsize=(14, 8))
 
-bars = plt.bar(filtered_df['Country'], filtered_df['Averaqe IQ'], color='skyblue')
+bars = plt.bar(filtered_df['Country'], filtered_df['Average IQ'], color='skyblue')
 
 
 plt.title("Average IQ by country (IQ >=100)", fontsize=16)
 
 plt.xlabel("Country", fontsize=14)
-plt.ylabel("Averaqe IQ", fontsize=14)
+plt.ylabel("Average IQ", fontsize=14)
 
 plt.xticks(rotation=90, fontsize=10)
 plt.yticks(fontsize=10)
 
-plt.grid(axis='y', lineStyles='--', alpha=0.8)
+plt.grid(axis='y', linestyle='--', alpha=0.8)
 
 plt.bar_label(bars, fmt='%.2f', fontsize=10, color='black')
 
