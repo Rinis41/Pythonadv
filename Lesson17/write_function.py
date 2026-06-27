068c4a3af48677a6939d47e24cf5a104945c477f
Rinis41 <86959494+Rinis41@users.noreply.github.com>
2026-06-27 14:29:06 +0200

    yes

---
+++ Lesson17/write_function.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame({
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [24, 27, 22]
+    'City': ['New York', 'San Francisco', 'Los Angelos']
+})
+
+st.write(df)
